5350462331f7d94e451350e38a9f32bfed3f8071
Amber Stone <51244364+amberstone2020@users.noreply.github.com>
2020-05-09 22:32:22 -0400

    initial commit

---
+++ PrintTwo.java
@@ -0,0 +1,22 @@
+package firstsemester;
+
+/*
+ * Amber Stone
+ * Date: 08.30.19
+ * Description: This program creates a diamond shape using asterisks.
+ */
+public class PrintTwo {
+
+    public static void main(String[] args) {
+        System.out.println("    *    ");
+        System.out.println("   ***   ");
+        System.out.println("  *****  ");
+        System.out.println(" ******* ");
+        System.out.println("*********");
+        System.out.println(" ******* ");
+        System.out.println("  *****  ");
+        System.out.println("   ***   ");
+        System.out.println("    *    ");
+
+    }
+}
