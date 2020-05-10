c1e2993301899897e68da33a4372bdd7df5a420a
Amber Stone <51244364+amberstone2020@users.noreply.github.com>
2020-05-09 22:29:36 -0400

    initial commit

---
+++ PrintOne.java
@@ -0,0 +1,18 @@
+/*
+ * Amber Stone
+ * Date: 08.30.19
+ * Description: This program prints PSCC name and address.
+ */
+package firstsemester;
+
+public class PrintOne {
+
+    public static void main(String[] args) {
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+        System.out.println("@ Pellissippi State Community College @");
+        System.out.println("@ 10915 Hardin Valley Road            @");
+        System.out.println("@ Knoxville, TN 37933                 @");
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+
+    }
+}
